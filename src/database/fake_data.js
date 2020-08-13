ba869496958c14f7efa1c5cfefaf3b353d620d7c
Eliseu Hairone <eliseuarviscoh@gmail.com>
2020-08-13 19:25:49 +0200

    Servidor conectado ao banco de dados

---
+++ src/database/fake_data.js
@@ -0,0 +1,35 @@
+const proffys = [{
+        name: "Eliseu Hairone",
+        avatar: "https://avatars0.githubusercontent.com/u/59767992?s=460&u=bf87ef4470d5874cbdda215eca57799f0c58d5ab&v=4",
+        whatsapp: "846077461",
+        bio: "Entusiasta das melhores tecnologias de informatica avançada. Apaixonado por desenvolver sistemas e por mudar a vida das pessoas através das tecnologia da informacao.",
+        subject: "Informatica",
+        cost: "500",
+        weekday: [
+            0
+        ],
+        time_from: [
+            720
+        ],
+        time_to: [
+            1220
+        ]
+    },
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "860554262",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "750",
+        weekday: [
+            1
+        ],
+        time_from: [
+            720
+        ],
+        time_to: [
+            1220
+        ]
+    }
+]
--- src/database/test.js
+++ src/database/test.js
@@ -11,7 +11,7 @@ Database.then(async(db) => {
     }
 
     classValue = {
-        subject: "Informatica",
+        subject: 1,
         cost: "500"
             // O proffyid vira pelo banco de dados
     }
@@ -30,7 +30,35 @@ Database.then(async(db) => {
         }
     ]
 
-    await createProffy(db, { proffyValue, classValue, classScheduleValues })
+    // await createProffy(db, { proffyValue, classValue, classScheduleValues })
 
     // Consultando dados
+
+    // Todos os proffys
+    const selectProffys = await db.all("SELECT * FROM proffys")
+        // console.log(selectProffys)
+
+    // Consultando as classes de um determinado professor e suas classes e dados
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+
+    // console.log(selectClassesAndProffys)
+
+    // O horario que a pessoa trabalha, por exemplo, e' das 8h - 18h
+    // O horario do time_from (8h) precisa ser antes ou igual ao horario solicitado
+    // O time_to precisa ser acima
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = 1
+        AND class_schedule.weekday = "1"
+        AND class_schedule.time_from <= "820"
+        AND class_schedule.time_to > "720";
+    `)
+
+    console.log(selectClassesSchedules)
 })
